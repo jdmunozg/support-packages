641ee2b7272b2165f244b37f7ed149d2aae898f5
jdmunozg <juan.david.m.g1004@gmail.com>
2023-10-24 11:35:26 -0500

    Feat(Create multi projects front)

--- support-packages-front/projects/dmsp/src/app/app.component.spec.ts
+++ support-packages-front/projects/dmsp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'support-packages-front'`, () => {
+  it(`should have as title 'dmsp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('support-packages-front');
+    expect(app.title).toEqual('dmsp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('support-packages-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dmsp app is running!');
   });
 });
